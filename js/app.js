7fd2fba8f7d266a2c9aaa84ec11c4a4aeca01a0d
Ahmed Al Ansaary <ahansaary@gmail.com>
2016-04-24 09:06:15 +0200

    Added UI improvements and Legend for layers

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var selectedLayers = {};
 $(document).ready(function () {
     // Create latitude and longitude and convert them to default projection
     var egypt = ol.proj.transform([29.8206, 23.8025], 'EPSG:4326', 'EPSG:3857');
@@ -43,7 +44,7 @@ $(document).ready(function () {
     });
 
     // toggle base layer
-    var $toggleBaseLayer = $('<button class="toggleBaseLayer">Satellite Map</button>');
+    var $toggleBaseLayer = $('<button class="toggleBaseLayer button button-clear">Satellite Map</button>');
     $menu.append($toggleBaseLayer);
 
     $toggleBaseLayer.click(function() {
@@ -56,9 +57,10 @@ $(document).ready(function () {
             map.addLayer(osm);
             $(this).text("Satellite Map");
         }
+        $('.menu').fadeOut(300);
     });
 
-    var $addLayer = $('<button class="addLayer">Add Layer</button>');
+    var $addLayer = $('<button class="addLayer button button-clear">Add Layer</button>');
     $menu.append($addLayer);
 
     var layersJson = [];
@@ -87,9 +89,9 @@ $(document).ready(function () {
     });
 
     $menu.on('change', '.layerCheckBox', function() {
+        var layers = layersJson[$(this).data('index')].layers;
         if (this.checked){
-            var layers = layersJson[$(this).data('index')].layers,
-                $subLayers = $('<div class="subLayers"></div>'),
+            var $subLayers = $('<div class="subLayers"></div>'),
                 $ul = $('<ul></ul>');
             for (var layer in layers) {
                 if (layers.hasOwnProperty(layer)) {
@@ -99,7 +101,44 @@ $(document).ready(function () {
             }
             $(document.body).append($subLayers);
         } else {
-            //
+            for (var layer in layers) {
+                if (layers.hasOwnProperty(layer)) {
+                    var layersArray = layers[layer].toString().split(',');
+                    if(layersArray.length > 1) {
+                        layersArray.forEach(function(e) {
+                            map.removeLayer(selectedLayers[e]);
+                            $('.legend[data-layer="'+e+'"]').remove();
+                        });
+                    } else {
+                        map.removeLayer(selectedLayers[layers[layer]]);
+                        $('.legend[data-layer="'+layers[layer]+'"]').remove();
+                    }
+                    $('.menu').fadeOut(300);
+                }
+            }
+
+        }
+    });
+
+    $(document.body).on('change', '.subLayerCheckBox', function() {
+        if (this.checked) {
+            var layer = $(this).data('layer');
+            var layersArray = layer.split(',');
+            if(layersArray.length > 1) {
+                var $subLayers = $('<div class="subLayers"></div>'),
+                    $ul = $('<ul></ul>');
+                layersArray.forEach(function(e) {
+                    $ul.append('<li><label><input data-layer="'+e+'" class="subLayerCheckBox" type="checkbox"> '+e+'</label></li>');
+                    $subLayers.append($ul);
+                });
+                $(document.body).append($subLayers);
+
+            } else {
+                addNewWMSLayers(map, null, layer);
+                getLayerLegend(layer);
+                $('.subLayers').remove();
+                $('.menu').fadeOut(300);
+            }
         }
     });
 
@@ -109,16 +148,23 @@ $(document).ready(function () {
     // newLayer = addNewWMSLayers(map, null, ['gpw-v3:gpw-v3-population-density_2000']);
 });
 
-function addNewWMSLayers(map, url, layers) {
+function addNewWMSLayers(map, url, layer) {
     if (url == null) {url = 'http://sedac.ciesin.columbia.edu/geoserver/wms'}
     var layerTile = new ol.layer.Tile({
         source: new ol.source.TileWMS({
             url: url,
             params: {
-                LAYERS: layers
+                LAYERS: layer
             }
         })
     });
     map.addLayer(layerTile);
+    selectedLayers[layer] = layerTile;
     return layerTile;
+}
+
+function getLayerLegend(layer) {
+    var legendUrl = "http://sedac.ciesin.columbia.edu/geoserver/wms?request=GetLegendGraphic&format=image%2Fpng&layer="+layer+"&width=15&height=15&legend_options=border:false;mx:0.05;my:0.02;dx:0.2;dy:0.07;fontSize:11;bandInfo:false;";
+    // $('.legend').remove();
+    $(document.body).append('<img data-layer="'+layer+'" class="legend" src="'+legendUrl+'">');
 }
